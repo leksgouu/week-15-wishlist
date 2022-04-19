8d75d8bd2c63b5f2fac4f2393120a57e5a13b433
Remi Ott <reott@tktk.ee>
2022-04-19 14:12:51 +0300

    wishlist

---
+++ model/wish.js
@@ -0,0 +1,48 @@
+const { json } = require("body-parser");
+const fs = require(`fs`);
+const path = require(`path`);
+const filepath = path.join(path.dirname(require.main.filename), `data`, `wishes.json`);
+
+module.exports = class wish {
+    constructor(wish){
+       this.description = wish; 
+    }
+
+    saveWish(){
+        fs.readFile(filepath,(error, filecontent)=>{
+            let wishes = [];
+
+            if(!error){
+                wishes = JSON.parse(filecontent);
+
+            }else{
+                console.log(error);
+            }
+
+            wishes.push(this);
+
+            fs.writeFile(filepath, JSON.stringify(wishes), (error) =>{
+                if(!error){
+                    console.log(`wish saved`);
+                }else{
+                    console.log(error);
+                }
+        
+            })
+
+
+
+        });
+    }
+ 
+    static fetchAllWishes(callBack){
+        fs.readFile(filepath, (error, filecontent) =>{
+            if(error){
+                callBack([]);
+            };
+            
+            callBack(JSON.parse(filecontent));
+        });
+
+    }
+}
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const wish = require('./model/wish');
+
+const app = express();
+
+app.set(`view engine`, ` ejs`);
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(`public`));
+
+app.get(`/`, (req, res) => {
+    wish.fetchAllWishes(wishesfromfile => {
+        console.log(wishesfromfile);
+        res.render(`index`, {mywishes: wishesfromfile});
+    });
+
+
+
+});
+
+app.post (`/wish`, (reg, res)=>{
+    let userData = reg.body.userwish;
+
+    let newwish = new Wish(userData);
+    newwish.savewish();
+    res.redirect(`/`);
+
+
+});
+
+
+const port = 5000;
+
+
+app.listen(port, () => {
+    console.log(`server is running ${port}.` );
+});
